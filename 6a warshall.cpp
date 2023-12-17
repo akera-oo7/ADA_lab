c33f724e76df822d5dc01a1323c01fd6f45bfff3
akera-oo7 <120010517+akera-oo7@users.noreply.github.com>
2023-12-17 18:03:14 +0530

    Update 6a warshall.c

--- 6a warshall.cpp
+++ 6a warshall.cpp
@@ -1,54 +1,40 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
+#include<math.h>
+int max(int,int);
+void warshal(int p[10][10],int n) {
+int i,j,k; for(k=1;k<=n;k++)
+  for(i=1;i<=n;i++)
+   for(j=1;j<=n;j++)
+p[i][j]=max(p[i][j],p[i][k]&&p[k][j]); }
+int max(int a,int b) {; if(a>b)
+ return(a);
+else
+return(b); }
+void main() {
+int p[10][10]={0},n,e,u,v,i,j;
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
+printf("\n Enter the number of vertices:"); scanf("%d",&n);
+printf("\n Enter the number of edges:"); scanf("%d",&e);
+for(i=1;i<=e;i++)
+   
+{
+ printf("\n Enter the end vertices of edge %d:",i);
+ scanf("%d%d",&u,&v);
+ p[u][v]=1;
 }
-
-void warshall(int p[10][10], int n) {
-    for (int k = 1; k <= n; k++)
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                p[i][j] = max(p[i][j], p[i][k] && p[k][j]);
+printf("\n Matrix of input data: \n");
+for(i=1;i<=n;i++)
+{
+ for(j=1;j<=n;j++)
+  printf("%d\t",p[i][j]);
+ printf("\n");
 }
-
-int main() {
-    int p[10][10] = {0}, n, e, u, v, i, j;
-
-    cout << "\nEnter the number of vertices: ";
-    cin >> n;
-    cout << "\nEnter the number of edges: ";
-    cin >> e;
-
-    for (i = 1; i <= e; i++) {
-        cout << "\nEnter the end vertices of edge " << i << ": ";
-        cin >> u >> v;
-        p[u][v] = 1;
-    }
-
-    cout << "\nMatrix of input data: \n";
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++)
-            cout << p[i][j] << "\t";
-        cout << "\n";
-    }
-    clock_t start = clock();
-  
-    warshall(p, n);
-
-    clock_t end = clock();
-    
-    
-
-    cout << "\nTransitive closure: \n";
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++)
-            cout << p[i][j] << "\t";
-        cout << "\n";
-    }
-    double execution_time = double(end - start) / double(CLOCKS_PER_SEC);
-
-    // Display execution time
-	cout << "Time taken by function: " << fixed<< execution_time << " seconds" << endl;
-    return 0;
+warshal(p,n);
+printf("\n Transitive closure: \n");
+for(i=1;i<=n;i++)
+{
+  for(j=1;j<=n;j++)
+   printf("%d\t",p[i][j]);
+  printf("\n");
+ }
 }
