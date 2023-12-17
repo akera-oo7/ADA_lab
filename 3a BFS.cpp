d8442fb3387b8ba04ff6e9284fd86927d47e3757
akera-oo7 <120010517+akera-oo7@users.noreply.github.com>
2023-12-17 17:40:28 +0530

    Update 3a BFS.c

--- 3a BFS.cpp
+++ 3a BFS.cpp
@@ -1,62 +1,41 @@
-#include <iostream>
-#include <queue>
-using namespace std;
 
-void BFS(int a[20][20], int source, int visited[20], int n);
-
-int main() {
-    int n, a[20][20], i, j, visited[20] = {0}, source;
-
-    cout << "Enter the number of vertices: ";
-    cin >> n;
-
-    cout << "\nEnter the adjacency matrix:\n";
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            cin >> a[i][j];
-
-    cout << "\nEnter the source node: ";
-    cin >> source;
-
-    if (source < 0 || source >= n) {
-        cout << "Invalid source node!";
-        return 1;  // Exit with an error code
-    }
-
-    visited[source] = 1;
-    clock_t start = clock();
-  
-    BFS(a, source, visited, n);
-
-    clock_t end = clock();
-
-    cout << "\nReachability of nodes from node " << source << ":\n";
-    for (i = 0; i < n; i++) {
-        if (visited[i] != 0)
-            cout << "Node " << i << " is reachable\n";
-        else
-            cout << "Node " << i << " is not reachable\n";
-    }
-    double execution_time = double(end - start) / double(CLOCKS_PER_SEC);
-
-    // Display execution time
-	cout << "Time taken by function: " << fixed<< execution_time << " seconds" << endl;
-    return 0;
+void BFS(int [20][20],int,int [20],int); 
+void main()
+{
+int n,a[20][20],i,j,visited[20],source;
+printf("Enter the number of vertices:"); 
+scanf("%d",&n);
+printf("\nEnter the adjacency matrix:\n"); 
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++) 
+scanf("%d",&a[i][j]); 
+for(i=1;i<=n;i++)
+visited[i]=0;
+printf("\nEnter the source node:"); 
+scanf("%d",&source); 
+visited[source]=1;
+BFS(a,source,visited,n);
+for(i=1;i<=n;i++)
+{
+if(visited[i]!=0)
+printf("\n Node %d is reachable",i);
+else
+printf("\n Node %d is not reachable",i);
 }
+}
+void BFS(int a[20][20],int source,int visited[20],int n) {
+int queue[20],f,r,u,v;
+f=0;
 
-void BFS(int a[20][20], int source, int visited[20], int n) {
-    queue<int> q;
-    q.push(source);
-
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-
-        for (int v = 0; v < n; v++) {
-            if (a[u][v] == 1 && visited[v] == 0) {
-                q.push(v);
-                visited[v] = 1;
-            }
-        }
-    }
+r=-1;
+queue[++r]=source; while(f<=r)
+{
+u=queue[f++]; for(v=1;v<=n;v++)
+{
+if(a[u][v]==1 && visited[v]==0) {
+queue[++r]=v;
+visited[v]=1;
+}
+}
+}
 }
