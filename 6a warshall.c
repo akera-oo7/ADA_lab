e45e77c8c5bca1916f06acaf91d2554e54b096f3
akera-oo7 <120010517+akera-oo7@users.noreply.github.com>
2024-03-12 18:15:34 +0530

    6a warshall.c

--- 6a warshall.c
+++ 6a warshall.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 #include<math.h>
 int max(int,int);
-void warshal(int p[10][10],int n) {
+void warshal(int p[10][10],int n) 
+{
 int i,j,k; for(k=1;k<=n;k++)
   for(i=1;i<=n;i++)
    for(j=1;j<=n;j++)
-p[i][j]=max(p[i][j],p[i][k]&&p[k][j]); }
-int max(int a,int b) {; if(a>b)
+p[i][j]=max(p[i][j],p[i][k]&&p[k][j]); 
+}
+int max(int a,int b)
+{ 
+ if(a>b)
  return(a);
 else
-return(b); }
-void main() {
+return(b); 
+}
+void main() 
+{
 int p[10][10]={0},n,e,u,v,i,j;
 
 printf("\n Enter the number of vertices:"); scanf("%d",&n);
